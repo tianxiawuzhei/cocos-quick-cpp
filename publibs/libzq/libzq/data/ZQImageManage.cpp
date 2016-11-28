34b3979fb8a25a85b985e838dc63cb0a0f77161b
zhaoyuqi <1064639871@qq.com>
2016-11-28 20:19:05 +0800

    [[ 19 ]]  ---  新加图片资源管理类

--- publibs/libzq/libzq/data/ZQImageManage.cpp
+++ publibs/libzq/libzq/data/ZQImageManage.cpp
@@ -7,3 +7,55 @@
 *
 ***********************************************/
 
+#include "ZQImageManage.h"
+#include "data/ZQFileManage.h"
+#include "log/ZQLogger.h"
+
+using namespace zq;
+
+ZQImageManage* ZQImageManage::getInstance()
+{
+    static ZQImageManage instance;
+    return &instance;
+}
+
+cocos2d::SpriteFrame* ZQImageManage::loadImage(const std::string &path, const std::string &key)
+{
+    const std::string &cache_key = key.empty() ? image : key;
+    auto texture = cocos2d::Director::getInstance()->getTextureCache()->getTextureForKey(cache_key);
+    if (!texture)
+    {
+        auto data = ZQFileManage::getInstance()->getDataFromFile(path);
+        if (data.isNull())
+        {
+            ZQLogE("loadImage: image file not found : %s ", path.c_str());
+            return nullptr;
+        }
+        
+        auto image = new (std::nothrow) Image();
+        if (!image)
+            return nullptr;
+        
+        bool bOk = image->initWithImageData(data.getBytes, data.getSize());
+        if (!bOk)
+        {
+            ZQLogE("loadImage: Load image from buffer error");
+            image->release();
+            return nullptr;
+        }
+        
+        cocos2d::Director::getInstance()->getTextureCache()->addImage(image, cache_key);
+        
+        texture = cocos2d::Director::getInstance()->getTextureCache()->getTextureForKey(cache_key);
+        if (!texture)
+        {
+            image->release();
+            return nullptr;
+        }
+        
+        image->release();
+    }
+    
+    auto size = texture->getContentSize();
+    return cocos2d::SpriteFrame::createWithTexture(texture, cocos2d::Rect(0, 0, size.width, size.height));
+}
--- publibs/libzq/libzq/data/ZQImageManage.h
+++ publibs/libzq/libzq/data/ZQImageManage.h
@@ -10,7 +10,23 @@
 #ifndef __ZQImageManage_H__
 #define __ZQImageManage_H__
 
-
+#include "cocos2d.h"
+namespace zq {
+    
+class ZQImageManage
+{
+public:
+    ZQImageManage* getInstance();
+    
+protected:
+    ZQImageManage() = default;
+    virtual ~ZQImageManage() = default;
+    
+public:
+    cocos2d::SpriteFrame* loadImage(const std::string &path, const std::string &key = "");
+};
+    
+}
 
 
 #endif /* defined(__ZQImageManage_H__) */
