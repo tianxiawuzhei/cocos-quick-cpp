139d860bc9424f5ef772edbd98e266d6314d4b4c
zhaoyuqi <1064639871@qq.com>
2016-12-16 17:52:28 +0800

    [[ 25 ]]  ---  修改日志输入方式，添加颜色

--- player/proj.mac/mac/ConsoleWindowController.m
+++ player/proj.mac/mac/ConsoleWindowController.m
@@ -43,39 +43,96 @@
         msg = [NSString stringWithFormat:@"%@ ...", [msg substringToIndex:MAX_LINE_LEN - 4]];
     }
     traceCount++;
-    NSFont *font = [NSFont fontWithName:@"Monaco" size:12.0];
-//    NSDictionary *attrsDictionary = [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName];
-    NSColor *txtColor = [NSColor blackColor];
-    if ([msg containsString:@"[ERROR]"]) {
-        txtColor = [NSColor redColor];
-    } else if ([msg containsString:@"[DEBUG]"]) {
-        txtColor = [NSColor blueColor];
+    
+    NSUInteger bPreMode = 0;
+    NSArray *array = [msg componentsSeparatedByString:@"\n"];
+    for (NSUInteger i = 0; i < [array count]; i++) {
+        NSString* item = [[array objectAtIndex:i] stringByAppendingString:@"\n"];
+        NSFont *font = [NSFont fontWithName:@"Monaco" size:12.0];
+        //    NSDictionary *attrsDictionary = [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName];
+        NSColor *txtColor = [NSColor blackColor];
+        if ([item containsString:@"[ERROR]"]) {
+            txtColor = [NSColor redColor];
+            bPreMode = 1;
+        } else if ([item containsString:@"[DEBUG]"]) {
+            txtColor = [NSColor blueColor];
+            bPreMode = 2;
+        } else {
+            if (bPreMode == 1) {
+                txtColor = [NSColor redColor];
+            } else if (bPreMode == 2) {
+                txtColor = [NSColor blueColor];
+            }
+        }
+        
+        NSFont *txtFont = [NSFont fontWithName:@"Monaco" size:12.0];
+        NSDictionary *attrsDictionary = [NSDictionary dictionaryWithObjectsAndKeys:txtFont,
+                                         NSFontAttributeName, txtColor,
+                                         NSForegroundColorAttributeName, nil];
+        
+        NSAttributedString *string = [[NSAttributedString alloc] initWithString:item attributes:attrsDictionary];
+        NSNumber *len = [NSNumber numberWithUnsignedInteger:1];
+        [linesCount addObject:len];
+        
+        //    [textView setTextColor:[NSColor redColor]];
+        NSTextStorage *storage = [textView textStorage];
+        [storage beginEditing];
+        [storage appendAttributedString:string];
+        
+        if ([linesCount count] >= MAX_LINES_COUNT)
+        {
+            len = [linesCount objectAtIndex:0];
+            [storage deleteCharactersInRange:NSMakeRange(0, [len unsignedIntegerValue])];
+            [linesCount removeObjectAtIndex:0];
+        }
+        
+        [storage endEditing];
+        [self changeScroll];
     }
     
-    NSFont *txtFont = [NSFont fontWithName:@"Monaco" size:12.0];
-    NSDictionary *attrsDictionary = [NSDictionary dictionaryWithObjectsAndKeys:txtFont,
-                          NSFontAttributeName, txtColor,
-                          NSForegroundColorAttributeName, nil];
-    
-    NSAttributedString *string = [[NSAttributedString alloc] initWithString:msg attributes:attrsDictionary];
-    NSNumber *len = [NSNumber numberWithUnsignedInteger:[string length]];
-    [linesCount addObject:len];
-
-//    [textView setTextColor:[NSColor redColor]];
-	NSTextStorage *storage = [textView textStorage];
-	[storage beginEditing];
-	[storage appendAttributedString:string];
-
-    if ([linesCount count] >= MAX_LINES_COUNT)
-    {
-        len = [linesCount objectAtIndex:0];
-        [storage deleteCharactersInRange:NSMakeRange(0, [len unsignedIntegerValue])];
-        [linesCount removeObjectAtIndex:0];
-    }
-
-	[storage endEditing];
-    [self changeScroll];
 }
+
+//- (void) trace:(NSString*)msg
+//{
+//    if (traceCount >= SKIP_LINES_COUNT && [msg length] > MAX_LINE_LEN)
+//    {
+//        msg = [NSString stringWithFormat:@"%@ ...", [msg substringToIndex:MAX_LINE_LEN - 4]];
+//    }
+//    traceCount++;
+//    
+//    NSFont *font = [NSFont fontWithName:@"Monaco" size:12.0];
+////    NSDictionary *attrsDictionary = [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName];
+//    NSColor *txtColor = [NSColor blackColor];
+//    if ([msg containsString:@"[ERROR]"]) {
+//        txtColor = [NSColor redColor];
+//    } else if ([msg containsString:@"[DEBUG]"]) {
+//        txtColor = [NSColor blueColor];
+//    }
+//    
+//    NSFont *txtFont = [NSFont fontWithName:@"Monaco" size:12.0];
+//    NSDictionary *attrsDictionary = [NSDictionary dictionaryWithObjectsAndKeys:txtFont,
+//                          NSFontAttributeName, txtColor,
+//                          NSForegroundColorAttributeName, nil];
+//    
+//    NSAttributedString *string = [[NSAttributedString alloc] initWithString:msg attributes:attrsDictionary];
+//    NSNumber *len = [NSNumber numberWithUnsignedInteger:[string length]];
+//    [linesCount addObject:len];
+//
+////    [textView setTextColor:[NSColor redColor]];
+//	NSTextStorage *storage = [textView textStorage];
+//	[storage beginEditing];
+//	[storage appendAttributedString:string];
+//
+//    if ([linesCount count] >= MAX_LINES_COUNT)
+//    {
+//        len = [linesCount objectAtIndex:0];
+//        [storage deleteCharactersInRange:NSMakeRange(0, [len unsignedIntegerValue])];
+//        [linesCount removeObjectAtIndex:0];
+//    }
+//
+//	[storage endEditing];
+//    [self changeScroll];
+//}
 
 - (void) changeScroll
 {
